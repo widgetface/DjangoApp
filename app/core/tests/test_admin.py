deb0fc10d1bfeb5e3b961e35df9cbb0b4a5aa2ba
davet <daveenq@gmail.com>
2020-07-14 16:41:03 +0100

    Added admin register app (cor) and the tests to test it

---
+++ app/core/tests/test_admin.py
@@ -0,0 +1,38 @@
+from django.test import TestCase, Client
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+
+class AdminSiteTest(TestCase):
+    # Its important this method is camelCase
+    def setUp(self):
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_superuser(
+            email='admin@test.com',
+            password='admin123'
+        )
+        # user helper function to login using django login
+        self.client.force_login(self.admin_user)
+        self.user = get_user_model().objects.create_user(
+            email='test@test.com',
+            password='test123',
+            name='Test user fullname'
+        )
+
+    def test_users_listed(self):
+        """ Test that users are listed on user page"""
+        # the usrl are defined in the django admin docs
+        # Generates a url for list user page in django admin section
+        url = reverse('admin:core_user_changelist')
+        res = self.client.get(url)
+
+        # Checks response object contains the following
+        self.assertContains(res, self.user.name)
+        self.assertContains(res, self.user.email)
+
+    def test_user_change_page(self):
+        """Test that the user edit page works"""
+        url = reverse('admin:core_user_change', args=[self.user.id])
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
